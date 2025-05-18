4d49b6a2cce791465c2b5f2c3aa873559df7ffaf
hyfvmitw <hyfvmitw@yandex.ru>
2025-05-18 08:06:28 +0500

    ...

--- js/stat.js
+++ js/stat.js
@@ -1,11 +1,53 @@
-window.renderStatistics = function (ctx) {
-    ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
-    ctx.fillRect(110, 20, 420, 270);
-    ctx.fillStyle = "#fff";
-    ctx.fillRect(100, 10, 420, 270);
+/* ---- Переменные для облака ----*/
 
-    ctx.font = "PT Mono bold 16px serif";
-    ctx.strokeText("Ура вы победили!", 120, 50);
-    ctx.strokeText("Список результатов:", 120, 70);
+let CLOUD_WIDTH = 420;  // Ширина облака
+let CLOUD_HEIGHT = 300; // Высота облака
+let CLOUD_X = 150; // Координата X облака
+let CLOUD_Y = 10; // Координата Y облака
+let GAP = 10;  // смещение тени облака
+
+/* ---- Переменные для текста ----*/
+let MESSAGE_WIN_X = 110
+let MESSAGE_WIN_Y = 50
+let MESSAGE_WIN_GAP = 20
+let FONT_COLOR = '#000'
+let FONT_FAMILY = '16px Arial'
+let FONT_GAP = 15
+let TEXT_WIDTH = 50
+
+/* ---- Переменные для гистаграммы ----*/
+let GISTAGRAMM_WIDTH = 150
+let GISTAGRAMM_HEIGHT = 20
+let GISTAGRAMM_Y = 100
+let GISTAGRAMM_GAP = 50
+
+
+
+let renderCloud = function (ctx, x, y, color) {
+    ctx.fillStyle = color;
+    ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
+}
+
+window.renderStatistics = function (ctx) {
+    renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.3)')
+    renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff')
+    ctx.textBaseline = 'top'
+    ctx.fillStyle = FONT_COLOR
+    ctx.font = FONT_FAMILY
+    ctx.fillText("Ура, вы победили!", CLOUD_X + GAP, MESSAGE_WIN_Y);
+    ctx.fillText("Список результатов:", CLOUD_X + GAP, MESSAGE_WIN_Y + MESSAGE_WIN_GAP);
+
+    ctx.fillText('Вы', CLOUD_X + GAP, GISTAGRAMM_Y)
+    ctx.fillRect(CLOUD_X + GAP + TEXT_WIDTH, GISTAGRAMM_Y, GISTAGRAMM_WIDTH, GISTAGRAMM_HEIGHT)
+
+    ctx.fillText('Кекс', CLOUD_X + GAP, GISTAGRAMM_Y + GISTAGRAMM_GAP)
+    ctx.fillRect(CLOUD_X + GAP + TEXT_WIDTH, GISTAGRAMM_Y + GISTAGRAMM_GAP, GISTAGRAMM_WIDTH, GISTAGRAMM_HEIGHT)
+
+    ctx.fillText('Катя', CLOUD_X + GAP, GISTAGRAMM_Y + GISTAGRAMM_GAP * 2)
+    ctx.fillRect(CLOUD_X + GAP + TEXT_WIDTH, GISTAGRAMM_Y + GISTAGRAMM_GAP * 2, GISTAGRAMM_WIDTH, GISTAGRAMM_HEIGHT)
+
+    ctx.fillText('Игорь', CLOUD_X + GAP, GISTAGRAMM_Y + GISTAGRAMM_GAP * 3)
+    ctx.fillRect(CLOUD_X + GAP + TEXT_WIDTH, GISTAGRAMM_Y + GISTAGRAMM_GAP * 3, GISTAGRAMM_WIDTH, GISTAGRAMM_HEIGHT)
+
 
 }
