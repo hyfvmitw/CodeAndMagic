bdef8c27356a990b866e671e2c5d50616e536dce
hyfvmitw <hyfvmitw@yandex.ru>
2025-05-22 00:03:24 +0500

    ...

--- js/stat.js
+++ js/stat.js
@@ -8,7 +8,7 @@ let GAP = 10;  // смещение тени облака
 
 /* ---- Переменные для текста ----*/
 let MESSAGE_WIN_X = 110
-let MESSAGE_WIN_Y = 50
+let MESSAGE_WIN_Y = 20
 let MESSAGE_WIN_GAP = 20
 let FONT_COLOR = '#000'
 let FONT_FAMILY = '16px Arial'
@@ -28,19 +28,45 @@ let renderCloud = function (ctx, x, y, color) {
     ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
 }
 
-window.renderStatistics = function (ctx, players) {
+let getMaxElement = function (arr) {
+    let maxElement = arr[0]
+    for (let i = 0; i < arr.length; i++)
+        if (arr[i] > maxElement) {
+            maxElement = arr[i]
+        }
+    return maxElement
+}
+
+let getMinElement = function (arr) {
+    let minElement = arr[0]
+    for (let i = 0; i < arr.length; i++)
+        if (arr[i] < minElement) {
+            minElement = arr[i]
+        }
+    return minElement
+}
+
+
+
+window.renderStatistics = function (ctx, players, times) {
     renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.3)')
     renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff')
     ctx.textBaseline = 'top'
     ctx.fillStyle = FONT_COLOR
     ctx.font = FONT_FAMILY
+
+    let minTime = Math.round(getMinElement(times))
+    console.log('minTime = ' + minTime);   
+
     ctx.fillText("Ура, вы победили!", CLOUD_X + GAP, MESSAGE_WIN_Y);
     ctx.fillText("Список результатов:", CLOUD_X + GAP, MESSAGE_WIN_Y + MESSAGE_WIN_GAP);
+    
+    let maxTime = Math.round(getMaxElement(times))
+    console.log('maxTime = ' + maxTime);   
 
     for (let i = 0; i < players.length; i++) {
         ctx.fillText(players[i], CLOUD_X + GAP, BAR_Y + BAR_GAP * i)
-        ctx.fillRect(CLOUD_X + GAP + TEXT_WIDTH, BAR_Y + BAR_GAP * i, BAR_WIDTH, BAR_HEIGHT)
+        ctx.fillRect(CLOUD_X + GAP + TEXT_WIDTH, BAR_Y + BAR_GAP * i, (BAR_WIDTH * times[i]) / maxTime, BAR_HEIGHT)
     }
 
-    
 }
